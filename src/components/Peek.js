f195220c076fe54b6ef5e289d2a9246d1996cf2b
Eugene Butler <eugene@eugene4.com>
2019-04-30 17:42:59 -0400

    csstransitions attempt

--- src/components/Peek.js
+++ src/components/Peek.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import { Transporter } from '../orbit'
 
 class Peek extends Component {
@@ -14,22 +15,31 @@ class Peek extends Component {
         ${peekedWork && "peek-work"}
       `}>
 
-        <Transporter name="caseStudy1" show={route !== 'work'}>
-          <div className="peek-case-study peek-case-study1" />
+      <TransitionGroup>
+        <Transporter name="caseStudy1" show={peekedWork}>
+          <CSSTransition in={peekPage == "work"} timeout={30} classNames="peek-case-study">
+            <div className="peek-case-study peek-case-study1"></div>
+          </CSSTransition>
         </Transporter>
 
-        <Transporter name="caseStudy2" show={route !== 'work'}>
-          <div className="peek-case-study peek-case-study2" />
+        <Transporter name="caseStudy2" show={peekedWork}>
+          <CSSTransition in={peekedWork} timeout={30} classNames="peek-case-study">
+            <div className="peek-case-study peek-case-study2" />
+          </CSSTransition>
         </Transporter>
 
-        <Transporter name="caseStudy3" show={route !== 'work'}>
-          <div className="peek-case-study peek-case-study3" />
+        <Transporter name="caseStudy3" show={peekedWork}>
+          <CSSTransition in={peekedWork} timeout={30} classNames="peek-case-study">
+            <div className="peek-case-study peek-case-study3" />
+          </CSSTransition>
         </Transporter>
 
-        <Transporter name="caseStudy4" show={route !== 'work'}>
-          <div className="peek-case-study peek-case-study4" />
+        <Transporter name="caseStudy4" show={peekedWork}>
+          <CSSTransition in={peekedWork} timeout={30} classNames="peek-case-study">
+            <div className="peek-case-study peek-case-study4" />
+          </CSSTransition>
         </Transporter>
-
+      </TransitionGroup>
       </div>
     )
   }
