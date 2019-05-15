d7773a22556a0759859152883617ceb96026c0c3
taylorz <taylor.zanke@gmail.com>
2019-05-15 15:53:46 -0400

    changes

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 import Nav from '../../components/Nav';
+import Peek from '../../Peek';
+import Work from '../work';
+import Betterfin from '../betterfin';
 
 class Homepage extends Component {
   state = {
     hovered: undefined,
     route: 'home'
+  }
 
+  updateHovered = (hovered) => {
+    this.setState({
+      hovered: hovered
+    })
+    console.log(hovered)
+  }
+  updateRoute = (route) => this.setState({ route })
+  renderRoute = () => {
+    const { route } = this.state
+    switch (route) {
+      case 'work':
+        return <Work navigate={this.updateRoute}/>
+      case 'betterfin':
+        return <Betterfin />
+      default:
+        return (
+          <div>
+            <h1 style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>homepage</h1>
+          </div>
+        )
+    }
   }
 
   render() {
     const { hovered, route } = this.state;
 
     return (
+      <>
         <Nav/>
+        <Peek peekPage={hovered} route={route} />
+        {this.renderRoute()}
+      </>
     )
   }
 }
