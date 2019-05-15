c356314315c5f635c9ef9032e5a76a027bba43ef
taylorz <taylor.zanke@gmail.com>
2019-05-15 17:21:36 -0400

    peeking

--- src/components/Peek.js
+++ src/components/Peek.js
@@ -18,7 +18,7 @@ class Peek extends Component {
         ${peekedWork && "peek-work"}
       `}>
       {route !== 'work' && <>
-        <Transporter name="caseStudy1" properties={['opacity']} show={peekedWork} duration={300}>
+        <Transporter name="caseStudy1" properties={['opacity']} show={peekedWork} duration={250}>
           <div className="peek-case-study peek-case-study1 peeked">
             <Grid container alignItems="flex-end">
               <Grid item md={12} xs={12}>
@@ -27,7 +27,7 @@ class Peek extends Component {
             </Grid>
           </div>
         </Transporter>
-        <Transporter name="caseStudy1" properties={['opacity']} show={!peekedWork} duration={300}>
+        <Transporter name="caseStudy1" properties={['opacity']} show={!peekedWork} duration={250}>
           <div className="peek-case-study peek-case-study1 unpeeked">
           <Grid container alignItems="flex-end">
             <Grid item md={12} xs={12}>
@@ -37,17 +37,17 @@ class Peek extends Component {
           </div>
         </Transporter>
 
-        <Transporter name="caseStudy2" properties={['opacity']} show={peekedWork} duration={300}>
+        <Transporter name="caseStudy2" properties={['opacity']} show={peekedWork} duration={100}>
           <div className="peek-case-study peek-case-study2 peeked" />
         </Transporter>
-        <Transporter name="caseStudy2" properties={['opacity']} show={!peekedWork} duration={300}>
+        <Transporter name="caseStudy2" properties={['opacity']} show={!peekedWork} duration={100}>
           <div className="peek-case-study peek-case-study2 unpeeked" />
         </Transporter>
 
-        <Transporter name="caseStudy3" properties={['opacity']} show={peekedWork} duration={300}>
+        <Transporter name="caseStudy3" properties={['opacity']} show={peekedWork} duration={150}>
           <div className="peek-case-study peek-case-study3 peeked" />
         </Transporter>
-        <Transporter name="caseStudy3" properties={['opacity']} show={!peekedWork} duration={300}>
+        <Transporter name="caseStudy3" properties={['opacity']} show={!peekedWork} duration={150}>
           <div className="peek-case-study peek-case-study3 unpeeked" />
         </Transporter>
         </>}
