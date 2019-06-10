e82d0cfdd045592104213abd5ec5060ba239b552
Eugene Butler <eugene@eugene4.com>
2019-06-10 17:25:38 -0400

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.261d40aee9de24332399466dc4412a78.js"
+  "/precache-manifest.f93a852ccea0db089a1d9e96ad6db2c5.js"
 );
 
 workbox.clientsClaim();
