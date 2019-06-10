aac4b7566abfd7411ea867318ed03da591d11b04
Eugene Butler <eugene@eugene4.com>
2019-06-10 17:23:39 -0400

    update scrolled element position even if animating

--- src/orbit.js
+++ src/orbit.js
@@ -142,7 +142,7 @@ export class Transporter extends React.Component {
     const { name, properties } = this.props
     const { anim } = this.state
     console.warn('scrolling', name, anim, anim === "shown",  this.childRef && this.childRef.current && ReactDOM.findDOMNode(this.childRef.current))
-    if (anim === "shown" && this.childRef && this.childRef.current && ReactDOM.findDOMNode(this.childRef.current)) {
+    if (this.childRef && this.childRef.current && ReactDOM.findDOMNode(this.childRef.current)) {
       nodes[name] = { // Set entry in nodes object
         styles: getStyles(ReactDOM.findDOMNode(this.childRef.current), properties),
         position: ReactDOM.findDOMNode(this.childRef.current).getBoundingClientRect()
